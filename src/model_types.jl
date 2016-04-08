c6d55078704aa3360d2e9aaf6e345cac0a4de1c0
Spencer Lyon <spencerlyon2@gmail.com>
2016-04-08 11:32:33 -0400

    Implemented new calibration type

--- src/model_types.jl
+++ src/model_types.jl
@@ -103,20 +103,113 @@ model_spec(sm::SymbolicModel) = sm.model_type
 # Calibration #
 # ----------- #
 
-# wrapper around ordered dict that will allow us to implement our own
-# getindex/setindex! methods
+immutable FlatCalibration <: Associative{Symbol,Float64}
+    d::OrderedDict{Symbol,Float64}
+end
+
+FlatCalibration(pairs::Pair{Symbol,Float64}...) =
+    FlatCalibration(OrderedDict(pairs))
+
+FlatCalibration() = FlatCalibration(OrderedDict{Symbol,Float64}())
+
+immutable GroupedCalibration <: Associative{Symbol,Vector{Float64}}
+    d::Dict{Symbol,Vector{Float64}}
+end
+
+GroupedCalibration() = GroupedCalibration(Dict{Symbol,Vector{Float64}}())
+
+GroupedCalibration(pairs::Pair{Symbol,Vector{Float64}}...) =
+    GroupedCalibration(Dict(pairs))
+
+# use let block so `Calib` isn't a member of the Dolo module
+let
+    typealias Calib Union{FlatCalibration,GroupedCalibration}
+
+    Base.getindex(c::Calib, n::Symbol) = c.d[n]
+    Base.getindex(c::Calib, nms::Symbol...) = [c[n] for n in nms]
+
+    # define the rest of the Associative interface by forwarding to c.d
+
+    # 1-arg functions
+    for f in [:length, :keys, :values, :keytype, :valtype, :eltype, :isempty,
+              :start]
+        @eval Base.$(f)(c::$(Calib)) = $(f)(c.d)
+    end
+
+    # 2-arg functions
+    for f in [:haskey, :delete!, :pop!, :sizehint!, :next, :done]
+        @eval Base.$(f)(c::$(Calib), arg) = $(f)(c.d, arg)
+    end
+
+    # 3-arg functions
+    for f in [:get, :get!, :getkey, :pop!]
+        @eval Base.$(f)(c::$(Calib), arg1, arg2) = $(f)(c.d, arg1, arg2)
+    end
+
+    # methods that didn't match signatures above
+    Base.get!(f::Function, c::Calib, key) = get!(f, c.d, key)
+    Base.get(f::Function, c::Calib, key) = get(f, c.d, key)
+    Base.merge!{T<:Calib}(c::T, others::T...) =
+        T(merge!(c.d, [o.d for o in others]...))
+    Base.show(io::IO, c::Calib) = show(io, c.d)
+    Base.showdict(io::IO, c::Calib) = Base.showdict(io, c.d)
+    Base.copy{T<:Calib}(c::T) = T(copy(c.d))
+    Base.deepcopy{T<:Calib}(c::T) = T(deepcopy(c.d))
+end
+
+# setting a single value
+Base.setindex!(fc::FlatCalibration, v::Real, k::Symbol) =
+    fc.d[k] = convert(Float64, v)
+
+# setting multiple values
+function Base.setindex!(fc::FlatCalibration, vs::Union{Tuple,AbstractVector},
+                        ks::Symbol...)
+    if length(vs) != length(ks)
+        throw(DimensionMismatch("length of keys and values must be the same"))
+    end
+
+    for (v, k) in zip(vs, ks)
+        fc[k] = v
+    end
+    fc
+end
+
+# setting a single group
+function Base.setindex!(gc::GroupedCalibration, v::AbstractVector, k::Symbol)
+    # use get with default of v in case we are adding a new key
+    if length(v) != length(get(gc, k, v))
+        msg = string("length $(length(v)) of new values for key $k does",
+                     "not match length of existing value ($(length(gc[k])))")
+        throw(DimensionMismatch(msg))
+    end
+    gc.d[k] = convert(Vector{Float64}, v)
+end
+
+# setting multiple groups
+function Base.setindex!(gc::GroupedCalibration, vs::Tuple, ks::Symbol...)
+    if length(vs) != length(ks)
+        throw(DimensionMismatch("length of keys and values must be the same"))
+    end
+
+    for (v, k) in zip(vs, ks)
+        gc[k] = v
+    end
+    gc
+end
+
+# Contains a FlatCalibration and GroupedCalibration, plus information on symbols
 immutable ModelCalibration
-    flat::OrderedDict{Symbol,Float64}
-    grouped::Dict{Symbol,Vector{Float64}}
+    flat::FlatCalibration
+    grouped::GroupedCalibration
     symbol_table::Dict{Symbol,Tuple{Symbol,Int}}
     symbol_groups::OrderedDict{Symbol,Vector{Symbol}}
 end
 
 function ModelCalibration(sm::SymbolicModel)
-    flat = solve_triangular_system(sm)
-    grouped = Dict{Symbol,Vector{Float64}}()
+    flat = FlatCalibration(solve_triangular_system(sm))
+    grouped = GroupedCalibration()
     for (k, nms) in sm.symbols
-        grouped[k] = [flat[nm] for nm in nms]
+        grouped[k] = Float64[flat[nm] for nm in nms]
     end
 
     symbol_table = Dict{Symbol,Tuple{Symbol,Int}}()
@@ -138,54 +231,34 @@ for f in (:copy, :deepcopy)
                          $(f)(mc.symbol_table), $(f)(mc.symbol_groups))
 end
 
-# TODO: Decide if we should keep these semantics. Right now I've implemented
-#       it so that if we do mc[x::Symbol] we try to extract the calibrated
-#       value for a single parameter as a Float64. Then if we call
-#       mc[s::AbstractString] we try to extract a whole symbol group as a
-#       Vector{Float64}
-Base.getindex(mc::ModelCalibration, n::Symbol) = mc.flat[n]
-Base.getindex(mc::ModelCalibration, n::AbstractString) = mc.grouped[symbol(n)]
-
-# now define methods that let us extract multiple params or groups at a time
+# getindex and setindex! should be forwarded on to the grouped field
+Base.getindex(mc::ModelCalibration, n::Symbol) = mc.grouped[n]
 Base.getindex(mc::ModelCalibration, nms::Symbol...) = [mc[n] for n in nms]
 
-# define this one with n1, nms... to avoid method ambiguity with previous
-# method above that has just nms::Symbol...
-Base.getindex(mc::ModelCalibration, n1::AbstractString, nms::AbstractString...) =
-    Vector{Float64}[mc[n] for n in vcat(n1, nms...)]
-
-# setting a single value
-function Base.setindex!(mc::ModelCalibration, v::Real, k::Symbol)
-    # update in flat
-    mc.flat[k] = convert(Float64, v)
-
-    # update in grouped
-    grp, ix = mc.symbol_table[k]
-    mc.grouped[grp][ix] = v
-end
-
-# setting multiple values
-function Base.setindex!(mc::ModelCalibration, vs::AbstractVector, ks::Symbol...)
-    if length(vs) != length(ks)
-        error("length of keys and values must be the same")
+function Base.setindex!(mc::ModelCalibration, v::AbstractVector, k::Symbol)
+    ks = mc.symbol_groups[k]
+    if length(v) != length(ks)
+        msg = string("Calibration has $(length(ks)) symbols in $k, ",
+                     "but passed $(length(v)) values")
+        throw(DimensionMismatch(msg))
     end
 
-    for (v, k) in zip(vs, ks)
-        mc[k] = v
+    # update grouped
+    mc.grouped[k] = v
+
+    # update flat
+    for (vi, ki) in zip(v, ks)
+        mc.flat[ki] = vi
     end
     mc
 end
 
-# setting a single group
-function Base.setindex!(mc::ModelCalibration, v::AbstractVector, k::AbstractString)
-    ks = mc.symbol_groups[symbol(k)]
-    if length(v) != length(ks)
-        msg = string("Calibration has $(length(ks)) symbols in $k, ",
-                     "but passed $(length(v)) values")
-        error(msg)
+function Base.setindex!(mc::ModelCalibration, vs::Tuple, ks::Symbol...)
+    if length(vs) != length(ks)
+        throw(DimensionMismatch("length of keys and values must be the same"))
     end
 
-    for (v, k) in zip(v, ks)
+    for (k, v) in zip(ks, vs)
         mc[k] = v
     end
     mc
