8b3c253881e957624095d58f1787fc7cb8aa6ab0
Spencer Lyon <spencerlyon2@gmail.com>
2016-04-12 10:16:54 -0400

    added Requests to REQUIRE and put model_import tests in a test_set

--- test/model_import.jl
+++ test/model_import.jl
@@ -6,6 +6,7 @@ end
 
 # @test import_model("/home/pablo/Programming/econforge/dolo/examples/models/rbc.yaml")
 
-@test import_model("https://raw.githubusercontent.com/EconForge/dolo/master/examples/models/rbc.yaml")
-
-@test import_model("https://raw.githubusercontent.com/EconForge/dolo/master/examples/models/sudden_stop.yaml")
+@testset "Testing yaml_import" begin
+    @test import_model("https://raw.githubusercontent.com/EconForge/dolo/master/examples/models/rbc.yaml")
+    @test import_model("https://raw.githubusercontent.com/EconForge/dolo/master/examples/models/sudden_stop.yaml")
+end
