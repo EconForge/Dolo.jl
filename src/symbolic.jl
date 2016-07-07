9e7160122182580968bb936f319b7a44278eb826
Spencer Lyon <spencerlyon2@gmail.com>
2016-07-07 23:02:11 +0100

    Make room for definitions in SymbolicModel type

--- src/symbolic.jl
+++ src/symbolic.jl
@@ -3,27 +3,31 @@ immutable SymbolicModel{ID,kind} <: ASM{ID,kind}
     equations::OrderedDict{Symbol,Vector{Expr}}
     calibration::OrderedDict{Symbol,Union{Expr,Symbol,Number}}
     options::Dict{Symbol,Any}
+    definitions::OrderedDict{Symbol,Expr}
     model_type::Symbol
     name::UTF8String
     filename::UTF8String
 
     function SymbolicModel(recipe::Associative, symbols::Associative,
                            eqs::Associative, calib::Associative,
-                           options::Associative,
+                           options::Associative, defs::Associative,
                            name="modeldoesnotwork", filename="none")
         # prep symbols
         model_type = symbol(recipe[:model_spec])
         _symbols = OrderedDict{Symbol,Vector{Symbol}}()
-        for k in recipe[:symbols]
-            _symbols[symbol(k)] = Symbol[symbol(v) for v in get(symbols, k, [])]
+        for _ in recipe[:symbols]
+            k = Symbol(_)
+            _symbols[k] = Symbol[Symbol(v) for v in get(symbols, k, [])]
         end
 
         # prep equations: parse to Expr
         _eqs = OrderedDict{Symbol,Vector{Expr}}()
         for k in keys(recipe[:specs])
-            k == :arbitrage && continue  # we handle these separately
 
-            these_eq = get(eqs, string(k), [])
+            # we handle these separately
+            (k in [:arbitrage, :arbitrage_exp]) && continue
+
+            these_eq = get(eqs, k, [])
 
             # verify that we have at least 1 equation if section is required
             if !get(recipe[:specs][k], :optional, false)
@@ -34,14 +38,18 @@ immutable SymbolicModel{ID,kind} <: ASM{ID,kind}
             _eqs[k] = Expr[_to_expr(eq) for eq in these_eq]
         end
 
-        # handle the arbitrage, controls_lb, and controls_ub separately
+        # handle the arbitrage, arbitrage_exp, controls_lb, and controls_ub
         if haskey(recipe[:specs], :arbitrage)
-            c_lb, c_ub, arb = _handle_arbitrage(eqs["arbitrage"],
+            c_lb, c_ub, arb = _handle_arbitrage(eqs[:arbitrage],
                                                 _symbols[:controls])
             _eqs[:arbitrage] = arb
             _eqs[:controls_lb] = c_lb
             _eqs[:controls_ub] = c_ub
         end
+
+        # TODO: fixme. For now this throws or inserts a stub
+        haskey(eqs, :arbitrage_exp) && error("Don't know how to do this yet")
+        _eqs[:arbitrage_exp] = Expr[]
 
         # prep calib: parse to Expr, Symbol, or Number
         _calib  = OrderedDict{Symbol,Union{Expr,Symbol,Number}}()
@@ -50,12 +58,14 @@ immutable SymbolicModel{ID,kind} <: ASM{ID,kind}
                 if k == :shocks
                     _calib[nm] = 0.0
                 else
-                    _calib[nm] = _expr_or_number(calib[string(nm)])
+                    _calib[nm] = _expr_or_number(calib[nm])
                 end
             end
         end
 
-        new(_symbols, _eqs, _calib, options, model_type, name, filename)
+        _defs = OrderedDict{Symbol,Expr}([k=>_to_expr(v) for (k, v) in defs])
+
+        new(_symbols, _eqs, _calib, options, _defs, model_type, name, filename)
     end
 end
 
@@ -65,24 +75,33 @@ function Base.show(io::IO, sm::SymbolicModel)
     """)
 end
 
-function SymbolicModel(from_yaml::Dict, model_type::Symbol, filename="none")
+function SymbolicModel(data::Dict, model_type::Symbol, filename="none")
     # verify that we have all the required fields
     for k in ("symbols", "equations", "calibration")
-        if !haskey(from_yaml, k)
+        if !haskey(data, k)
             error("Yaml file must define section $k for DTCSCC model")
         end
     end
 
-    d = deepcopy(from_yaml)
+    d = _symbol_dict(deepcopy(data))
+    if haskey(d, :model_type)
+        model_type_data = pop!(d, :model_type)
+        if string(model_type_data) != string(model_type)
+            error(string("Supplied model type $(model_type) does not match ",
+                         "model_type from data $(model_type_data)"))
+        end
+    end
     recipe = RECIPES[model_type]
-    name = pop!(d, "name", "modeldoesnotwork")
-    id = gensym(name)
-    options = _symbol_dict(pop!(d, "options", Dict()))
-    out = SymbolicModel{id,model_type}(recipe, pop!(d, "symbols"),
-                                       pop!(d, "equations"),
-                                       pop!(d, "calibration"),
+    nm = pop!(d, :name, "modeldoesnotwork")
+    id = gensym(nm)
+    options = _symbol_dict(pop!(d, :options, Dict()))
+    defs = _symbol_dict(pop!(d, :definitions, Dict()))
+    out = SymbolicModel{id,model_type}(recipe, pop!(d, :symbols),
+                                       pop!(d, :equations),
+                                       pop!(d, :calibration),
                                        options,
-                                       name,
+                                       defs,
+                                       nm,
                                        filename)
 
     if !isempty(d)
