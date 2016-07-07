846c2bc39cef804045a1c82f40bfdc8440f333e3
Spencer Lyon <spencerlyon2@gmail.com>
2016-07-07 21:31:23 +0100

    Organize numeric types for grid, distribution, options

--- src/numeric.jl
+++ src/numeric.jl
@@ -1,52 +1,10 @@
-# ------------------- #
-# Numeric model types #
-# ------------------- #
-immutable Options
-    distribution
-    grid
-    other::Dict{Symbol,Any}  # TODO: shouldn't need. Just keeps stuff around
-end
+# ---------- #
+# Grid types #
+# ---------- #
 
-Options(;grid=nothing, distribution=nothing, other=Dict{Symbol,Any}()) =
-    Options(grid, distribution, other)
+abstract AbstractGrid
 
-
-function Options(sm::AbstractSymbolicModel, calib::ModelCalibration)
-    # numericize options
-    _options = eval_with(calib, deepcopy(sm.options))
-
-    _opts = Dict{Symbol,Any}()
-    other = Dict{Symbol,Any}()
-
-    # now construct Options object
-    for k in keys(_options)
-        data = pop!(_options, k)
-        if k == :grid
-            if data[:kind] == :Cartesian
-                _opts[:grid] = Cartesian(data)
-            else
-                m = "don't know how to handle grid of type $(data[:kind])"
-                error(m)
-            end
-        elseif k == :distribution
-            if data[:kind] == :Normal
-                n = length(calib[:shocks])
-                sigma = reshape(vcat(data[:sigma]...), n, n)
-                _opts[k] = Normal(_to_Float64(sigma))
-
-            else
-                m = "don't know how to handle distribution of type $(data[:kind])"
-                error(m)
-            end
-        else
-            other[k] = data
-        end
-    end
-
-    Options(;_opts..., other=other)
-end
-
-immutable Cartesian
+immutable Cartesian <: AbstractGrid
     a::Vector{Float64}
     b::Vector{Float64}
     orders::Vector{Int}
@@ -61,8 +19,70 @@ function Cartesian(stuff::Associative)
     Cartesian(stuff[:a], stuff[:b], stuff[:orders])
 end
 
-immutable Normal
+function _build_grid(data::Associative)
+    if data[:kind] == :Cartesian
+        return Cartesian(data)
+    else
+        m = "don't know how to handle grid of type $(data[:kind])"
+        error(m)
+    end
+end
+
+# ------------------ #
+# Distribution types #
+# ------------------ #
+
+abstract AbstractDistribution
+
+immutable Normal <: AbstractDistribution
     sigma::Matrix{Float64}
+end
+
+function _build_dist(data::Associative, calib::ModelCalibration)
+    if data[:kind] == :Normal
+        n = length(calib[:shocks])
+        sigma = reshape(vcat(data[:sigma]...), n, n)
+        return Normal(_to_Float64(sigma))
+    else
+        m = "don't know how to handle distribution of type $(data[:kind])"
+        error(m)
+    end
+end
+
+
+# ------------------- #
+# Numeric model types #
+# ------------------- #
+
+immutable Options{TD<:AbstractDistribution, TG<:AbstractGrid}
+    grid::TG
+    distribution::TD
+    other::Dict{Symbol,Any}  # TODO: shouldn't need. Just keeps stuff around
+end
+
+Options(;grid=nothing, distribution=nothing, other=Dict{Symbol,Any}()) =
+    Options(grid, distribution, other)
+
+function Options(sm::AbstractSymbolicModel, calib::ModelCalibration)
+    # numericize options
+    _options = eval_with(calib, deepcopy(sm.options))
+
+    _opts = Dict{Symbol,Any}()
+    other = Dict{Symbol,Any}()
+
+    # now construct Options object
+    for k in keys(_options)
+        data = pop!(_options, k)
+        if k == :grid
+            _opts[:grid] = _build_grid(data)
+        elseif k == :distribution
+            _opts[:distribution] = _build_dist(data, calib)
+        else
+            other[k] = data
+        end
+    end
+
+    Options(;_opts..., other=other)
 end
 
 immutable DTCSCCModel{ID} <: ANM{ID,:dtcscc}
