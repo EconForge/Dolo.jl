030ceb0bb2c8cdb5ec440d365550065a6dde6cea
pablo.winant@gmail.com <pablo.winant@gmail.com>
2016-09-18 12:30:43 +0100

    Moved compat yaml files + tests.

--- test/model_import.jl
+++ test/model_import.jl
@@ -11,7 +11,8 @@ end
     @test import_model(Pkg.dir("Dolo", "examples", "models", "rbc_dtcc_ar1.yaml"))
     @test import_model(Pkg.dir("Dolo", "examples", "models", "rbc_dtcc_mc.yaml"))
     @test import_model(Pkg.dir("Dolo", "examples", "models", "rbc_dtcc_iid.yaml"))
-    @test import_model(Pkg.dir("Dolo", "examples", "models", "rbc_dtmscc.yaml"))
+    @test import_model(Pkg.dir("Dolo", "examples", "models", "compat",  "rbc_dtmscc.yaml"))
+    @test import_model(Pkg.dir("Dolo", "examples", "models", "compat",  "rbc_dtcscc.yaml"))
 
     # TODO: re-enable once we can build objects of type MarkovChain
     # @test import_model("https://raw.githubusercontent.com/EconForge/dolo/144965224f432c9f467f0e667bc0cc4d77caf629/examples/models/sudden_stop.yaml")
