b823272c5fe595b0c8e77ebfb150d216fe1c0978
Anastasia Zh <Anastasia Zh>
2017-03-24 14:27:40 +0100

    Minor

--- test/test_simul.jl
+++ test/test_simul.jl
@@ -11,7 +11,16 @@ N = 1
 T=40
 @time dr = Dolo.time_iteration(model, verbose=true, maxit=10000)
 
+s0 = model.calibration[:states]
+e0 = model.calibration[:exogenous]
+irf = Dolo.response(model, dr, s0, :e_z; Impulse=0.03)
+Dolo.response(model, dr, s0, :e_z)
 
+
+
+
+
+############## 2 shocks
 filename2 = joinpath(path,"examples","models","rbc_dtcc_iid_2ar1.yaml")
 model2 = Dolo.yaml_import(filename2)
 @time dr2 = Dolo.time_iteration(model2, verbose=true, maxit=10000)
@@ -28,108 +37,18 @@ Dolo.response(model2, dr2, s0, e0, :e_d)
 
 
 
-
-
-
-sims = Dolo.simulate(model2, dr2, s0, e0, stochastic = true; n_exp=1, T=40 )  
-
-
-
-
-
-
-Impulse = 0.3
-index_s = findfirst(model2.symbols[:exogenous], :e_d)
-isempty(Impulse)
+index_s = findfirst(model.symbols[:exogenous], :e_z)
 if isempty(Impulse)
-  Impulse = sqrt(diag(model2.exogenous.Sigma)[index_s])
-end
-Impulse
-
-isempty(Impulse)
-if !isempty(Impulse)
-  Impulse = impulse_response
-else
   Impulse = sqrt(diag(model.exogenous.Sigma)[index_s])
 end
 
-
-
-
-
-
-
-
-
-
-### Simulation model
-
-
-
-
-# calculate initial controls using decision rule
-println(size(epsilons))
-x0 = dr(epsilons[1,:,1],s0)
-
-# get number of states and controls
-calib = model.calibration
-params = calib[:parameters]
-driving_process = m_simul
-driving_process
-!isempty(driving_process)
-epsilons=zeros(length(model.exogenous.mu), 1, T)
-if !isempty(driving_process)
-    for ii in 1:size(driving_process)[2]
-      epsilons[:,1,ii] = driving_process[:,ii]
-    end
-else
-      epsilons = Dolo.simulate(model.exogenous, 1, T, e0; stochastic=true)
-end
-epsilons
-
-ns = length(s0)
-nx = length(x0)
-nsx = nx+ns
-n_exp=1
-T=40
-# TODO: talk to Pablo and make a decision about this
-s_simul = Array(Float64, n_exp, ns, T)
-x_simul = Array(Float64, n_exp, nx, T)
-for i in 1:n_exp
-  s_simul[i, :, 1] = s0
-  x_simul[i, :, 1] = x0
-end
-# NOTE: this will be empty if ny is zero. That's ok. Our call to `cat`  #       below will work either way  y_simul = Array(Float64, n_exp, ny, horizon)
-epsilons
-
-t=1
-s = copy(view(s_simul, :, :, t))
-m = copy(view(epsilons, :, :, t))
-x = dr(m,s)
-x_simul[:, :, t] = x
-t
-M = view(epsilons, :, :, t+1)
-ss = view(s_simul, :, :, t+1)
-println([size(e) for e in [ss,m,s,x,M]])
-ss = Dolo.transition!(model, (ss), (m), (s), (x), (M), params)
-s_simul[:, :, t+1] = ss
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Impulse=zeros(0)
+Impulse
+isempty(Impulse)
+Impulse::Float64=vec(zeros(0, 0)
+vec(zeros(0, 0))
+zeros(0)
+Impulse::Float64
 
 
 
@@ -138,6 +57,7 @@ s_simul[:, :, t+1] = ss
 
 
 stochastic = false
+
 irf=true
 model
 model.exogenous
