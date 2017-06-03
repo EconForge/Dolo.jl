b3f5853345048ba7eb0b50c134c74fc3207edf54
Spencer Lyon <spencerlyon2@gmail.com>
2017-06-03 10:46:35 -0400

    DOC: trying to get docs to build

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,9 @@
 using Documenter, Dolo
 
 makedocs(
+    modules=[Dolo],
+    authors="Spencer Lyon, Pablo Winant, and contributors",
+    clean=false,
     format=:html,
     sitename="Dolo.jl",
     pages=[
@@ -13,5 +16,7 @@ makedocs(
 deploydocs(
     repo   = "github.com/EconForge/Dolo.jl.git",
     julia  = "0.6",
-    osname = "linux"
+    target = "build",
+    deps   = nothing,
+    make   = nothing,
 )
