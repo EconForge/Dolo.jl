efa3558aafa2123f9dd1928d4091311095617190
autodocs <autodocs>
2017-06-26 14:31:30 +0000

    build based on 82e8bd3

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Dolo.jl",
+    "title": "Dolo.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Dolo.jl-1",
+    "page": "Dolo.jl",
+    "title": "Dolo.jl",
+    "category": "section",
+    "text": "This is just a placeholder"
+},
+
+]}
