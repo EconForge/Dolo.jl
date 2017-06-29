7f63682727a987cc9007edea56491a72d1f63519
Spencer Lyon <spencerlyon2@gmail.com>
2017-06-28 23:27:35 -0400

    ENH: implement SmolyakGrid properly

--- src/numeric/grids.jl
+++ src/numeric/grids.jl
@@ -49,19 +49,21 @@ n_nodes(grid::Grid) = size(grid.nodes, 1)
 node(grid::Grid, i::Int) = grid.nodes[i, :]
 
 immutable SmolyakGrid <: Grid
-
-    min::Vector{Float64}
-    max::Vector{Float64}
-    mu::Vector{Int}
+    smol_params::BM.SmolyakParams{Float64,Vector{Int}}
     nodes::Matrix{Float64}
 
-    function SmolyakGrid(min::Array{Float64,1}, max::Array{Float64,1}, mu::Int64)
-        return new(min, max, [mu for i=1:length(min)])
-    end
+    function SmolyakGrid(min::Vector{Float64}, max::Vector{Float64}, mu::Vector{Int})
+        d = length(min)
+        dim_err = DimensionMismatch("min was length $d, max and mu must match")
+        length(max) == d || throw(dim_err)
+        length(mu) == d || throw(dim_err)
 
-    function SmolyakGrid(min::Array{Float64,1}, max::Array{Float64,1}, mu::Array{Int64,1})
-        nodes = zeros(10, 10) # TODO !!!!
-        return new(min, max, mu, nodes)
+        sp = BM.SmolyakParams(d, mu, min, max)
+        nodes = BM.nodes(sp)
+        return new(sp, nodes)
     end
+end
 
+function SmolyakGrid(min::Array{Float64,1}, max::Array{Float64,1}, mu::Int)
+    return SmolyakGrid(min, max, fill(mu, length(min)))
 end
