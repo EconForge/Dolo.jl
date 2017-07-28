f50f69a55ea72edf43ea750f1987e7fd6c092281
ezgioz <ezgiozsogut@gmail.com>
2017-07-28 12:50:58 +0100

    Edited dictionary type as more general

--- src/algos/perfect_foresight.jl
+++ src/algos/perfect_foresight.jl
@@ -119,7 +119,7 @@ function perfect_foresight(model, exo::AbstractMatrix{Float64}; T=200, verbose=t
 end
 
 # Constructs the matrix exo given a dictionary exo and calls the original method
-function perfect_foresight(model, exo::Dict{Symbol,Array{Float64,1}}; kwargs... )
+function perfect_foresight(model, exo::Dict{Symbol,Array{Real,1}}; kwargs... )
 
   n_e = length(model.symbols[:exogenous])
   T_e = maximum(length(e) for e in values(exo))
