878d29d34b686fd1602d0e84f7d07a7c0c7ebb41
Pablo Winant <pablo.winant@gmail.com>
2017-08-29 14:40:59 +0100

    TRAVIS: everything was so perfect in theory...

--- src/Dolo.jl
+++ src/Dolo.jl
@@ -67,7 +67,7 @@ id{ID}(::AbstractModel{ID}) = ID
 @compat ListOfPoints{d} = Vector{Point{d}}
 @compat ListOfValues{n} = Vector{Value{n}}
 
-vector_to_matrix(v::Vector) = Matrix(vec(v)')
+vector_to_matrix(v) = Matrix(vec(v)')
 # vector_to_matrix(v::Vector) = Matrix(v')
 # vector_to_matrix(v::RowVector) = Matrix(v)
 
