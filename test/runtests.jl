fea1e01f9312dff8fcd1b8688c704d0439131066
Pablo Winant <pablo.winant@gmail.com>
2017-11-21 01:36:42 +0000

    WIP: proudly breaking all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,17 @@ module DoloTests
 using Dolo, DataStructures, Base.Test
 
 tests = length(ARGS) > 0 ? ARGS : [
-                                   "model_types",
-                                   "model_import",
-                                   "test_calibration",
-                                   "test_algos",
-                                   "test_perfect_foresight",
-                                   "test_features",
-                                   "test_linter",
-                                   "test_minilang",
+                                   # "model_types",
+                                   # "model_import",
+                                   # "test_calibration",
+                                   # "test_algos",
+                                   # "test_perfect_foresight",
+                                   # "test_features",
+                                   # "test_linter",
+                                   # "test_minilang",
                                    "test_decision_rules",
-                                   "test_discretize_process"]
+                                   # "test_discretize_process"
+                                   ]
 
 for t in tests
     include("$(t).jl")
