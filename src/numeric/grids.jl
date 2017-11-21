1437e268fe1a6d00ec53bb83313130caee7154a6
Pablo Winant <pablo.winant@gmail.com>
2017-11-21 13:11:19 +0000

    Move grids.jl to points/list-of-points

--- src/numeric/grids.jl
+++ src/numeric/grids.jl
@@ -2,20 +2,12 @@
 #
 
 # # backward backward compatibility
-nodes(::Type{<:Matrix}, grid::Grid{d}) where  d  = from_LOP(nodes(grid))
+nodes(::Type{<:Union{ListOfPoints,ListOfPoints{d}}}, grid::Grid{d}) where d = nodes(grid)
+nodes(::Type{<:Matrix}, grid::Grid) = from_LOP(nodes(grid))
 
-# nodes(::Type{Array{Float64,2}}, grid::Grid) = from_LOP(nodes(grid))
+node(::Type{<:Union{Point,Point{d}}}, grid::Grid{d}, i::Int) where d = node(grid,i)
+node(::Type{<:Vector}, grid::Grid, i::Int) = Vector(node(grid,i))
 
-# node(::typeof(Point), grid::Grid{d}, i::Int) where d= SVector{d,Float64}(node(grid, i)...)
-# inode(::typeof(Point), grid::Grid{d}, i::Int) where d = SVector{d,Float64}(inode(grid, i)...)
-
-
-
-# # # type is not predictible yet
-nodes(::Type{<:ListOfPoints}, grid::Grid{d}) where  d = (nodes(grid))
-# node(::typeof(Point), grid::Grid{d}, i::Int) where d= SVector{d,Float64}(node(grid, i)...)
-# inode(::typeof(Point), grid::Grid{d}, i::Int) where d = SVector{d,Float64}(inode(grid, i)...)
-#
 
 
 import Base
@@ -71,6 +63,7 @@ end
 nodes(grid::UnstructuredGrid) = grid.nodes
 n_nodes(grid::UnstructuredGrid) = length(grid.nodes)
 node(grid::UnstructuredGrid, i::Int) = grid.nodes[i] # fail if i!=1 ?
+node(::Type{<:Point}, grid::UnstructuredGrid, i::Int) = node(grid,i)
 
 function Product(a::UnstructuredGrid{d1}, b::UnstructuredGrid{d2}) where d1 where d2
     A = [Base.product(a.nodes, b.nodes)...]
@@ -119,44 +112,75 @@ end
 
 
 
-immutable SmolyakGrid{N} <: Grid{N}
+immutable SmolyakGrid{d} <: Grid{d}
     smol_params::BM.SmolyakParams{Float64,Vector{Int}}
     nodes::Matrix{Float64}
     B_nodes::Matrix{Float64}
-
-    function (::Type{SmolyakGrid{N}}){N}(min::Vector{Float64}, max::Vector{Float64}, mu::Vector{Int})
-        d = length(min)
-        @assert d == N
-        dim_err = DimensionMismatch("min was length $d, max and mu must match")
-        length(max) == d || throw(dim_err)
-        length(mu) == d || throw(dim_err)
-
-        sp = BM.SmolyakParams(d, mu, min, max)
-        nodes = BM.nodes(sp)
-        B_nodes = BM.evalbase(sp, nodes)
-        return new{N}(sp, nodes, B_nodes)
-    end
-    function (::Type{SmolyakGrid{N}}){N}(min::Array{Float64,1}, max::Array{Float64,1}, mu::Int)
-        return SmolyakGrid{N}(min, max, fill(mu, N))
-    end
 end
 
-
-immutable RandomGrid{N} <: Grid{N}
-    min::Vector{Float64}
-    max::Vector{Float64}
-    n::Int
-    nodes::Matrix{Float64}
-    function (::Type{RandomGrid{N}}){N}(min, max, n)
-        d = length(min)
-        @assert d == N
-        dim_err = DimensionMismatch("min was length $d, max must be also")
-        length(max) == d || throw(dim_err)
-        all(max .> min) || error("max must be greater than min")
-
-        nodes = rand(n, d)  # on [0, 1]
-        nodes .= nodes .* (max - min)' .+ min' # scale/shift to [min, max]
-
-        return new{N}(min, max, n, nodes)
-    end
+function (::Type{<:SmolyakGrid})(min::Point{d}, max::Point{d}, mu::SVector{d,Int64}) where d
+    sp = BM.SmolyakParams(d, Vector(mu), Vector(min), Vector(max))
+    nodes = BM.nodes(sp)
+    B_nodes = BM.evalbase(sp, nodes)
+    return SmolyakGrid{d}(sp, nodes, B_nodes)
 end
+
+function (::Type{<:SmolyakGrid})(min::Point{d}, max::Point{d}, mu::Int64) where d
+    return SmolyakGrid(min, max, SVector([mu for i=1:d]...))
+end
+
+function (::Type{<:SmolyakGrid})(min::Vector{Float64},max::Vector{Float64},mu::Vector{Int64})
+    d = length(min)
+    @assert d == length(max) == length(mu)
+    SmolyakGrid(SVector(min...), SVector(max...), SVector(mu...))
+end
+
+function (::Type{<:SmolyakGrid})(min::Vector{Float64},max::Vector{Float64},mu::Int64)
+    d = length(min)
+    @assert d == length(max)
+    SmolyakGrid{d}(min, max, fill(mu,d))
+end
+
+nodes(grid::SmolyakGrid{d}) where d  = reinterpret(Point{d}, grid.nodes',( size(grid.nodes,1),))
+n_nodes(grid::SmolyakGrid) = size(grid.nodes,1)
+node(grid::SmolyakGrid{d}, i::Int) where d = Point{d}(grid.nodes[i,:]...)
+
+###############
+# Random Grid #
+###############
+
+immutable RandomGrid{d} <: Grid{d}
+    min::SVector{d,Float64}
+    max::SVector{d,Float64}
+    n::Int64
+    nodes::ListOfPoints{d}
+end
+
+function  (::Type{<:Union{RandomGrid,RandomGrid{d}}})(min::Point{d}, max::Point{d}, n::Int) where d
+    nodes = reinterpret(Point{d}, rand(d, n), (n,))  # on [0, 1]
+    for n=1:length(nodes)
+        nodes[n] = nodes[n] .* (max-min) + min
+    end
+    RandomGrid(min, max, n, nodes)
+end
+
+function (::Type{RandomGrid})(min::Vector{Float64}, max::Vector{Float64}, n::Int)
+    d = length(min)
+    @assert d == length(max)
+    dim_err = DimensionMismatch("min was length $d, max must be also")
+    length(max) == d || dim_err
+    all(max .> min) || error("max must be greater than min")
+    RandomGrid{d}(SVector(min...),SVector(max...),n)
+end
+
+function (::Type{RandomGrid{d}})(min::Vector{Float64}, max::Vector{Float64}, n::Int) where d
+    @assert d == length(min)
+    RandomGrid(min,max,d)
+end
+
+nodes(grid::RandomGrid) = grid.nodes
+nodes(::Type{<:ListOfPoints}, grid::RandomGrid) = nodes(grid)
+
+n_nodes(grid::RandomGrid) = length(grid.nodes)
+
+node(grid::RandomGrid,i::Int) = grid.nodes[i]
