2b4ca900d0b7b5ddb12ebc6732e5e5ccf686ec8a
Gabrielle Queran <gabrielle.queran@gmail.com>
2021-11-25 16:04:00 +0100

    creating a new trembling hand function called my_trembling_hand! in ergodic.jl

--- src/algos/ergodic.jl
+++ src/algos/ergodic.jl
@@ -161,98 +161,46 @@ function trembling_hand!(A::AbstractArray{Float64,3}, x, w)
     end
 end
 
-function my_trembling_hand_v1!(A::AbstractArray{Float64,2}, x::Vector{Point{d}}, w::Float64) where d
 
-    if d==1
-        N,n0 = size(A)
-        δ0 = 1.0./(n0-1.0)
-        for n in 1:N
-            x0 = x[n][1]
-            x0 = min.(max.(x0, 0.0),1.0)
-            q0 = div.(x0, δ0)
-            q0 = max.(0, q0)
-            q0 = min.(q0, n0-2)
-            λ0 = (x0./δ0-q0) # ∈[0,1[ by construction
-            q0_ = round.(Int,q0) + 1
-            A[n, q0_]   += (1-λ0)*w
-            A[n, q0_+1] += λ0*w
-        end
 
-    elseif d==2
-        N,n0,n1 = size(A)
-        δ0 = 1.0./(n0-1.0)
-        δ1 = 1.0./(n1-1.0)
-        for n in 1:N
-            x0 = x[n][1]
-            x0 = min.(max.(x0, 0.0),1.0)
-            q0 = div.(x0, δ0)
-            q0 = max.(0, q0)
-            q0 = min.(q0, n0-2)
-            λ0 = (x0./δ0-q0) # ∈[0,1[ by construction
-            q0_ = round.(Int,q0) + 1
-
-            x1 = x[n][2]
-            x1 = min.(max.(x1, 0.0),1.0)
-            q1 = div.(x1, δ1)
-            q1 = max.(0, q1)
-            q1 = min.(q1, n1-2)
-            λ1 = (x1./δ1-q1) # ∈[0,1[ by construction
-            q1_ = round.(Int,q1) + 1
-
-            A[n, q0_ ,  q1_] += (1-λ0)*(1-λ1)*w
-            A[n, q0_+1, q1_] += λ0*(1-λ1)*w
-            A[n, q0_, q1_+1] += (1-λ0)*λ1*w
-            A[n, q0_+1, q1_+1] += λ0*λ1*w
-        end
-
-    else
-        print("Error : grid's dimension limited to 2")
-    end
-
+function make_λn_weight_vector(λn::Point{d}) where d
+    return tuple( SVector(1-λn[i],λn[i]) for i in 1:d )
 end
 
-function my_trembling_hand_v2!(A, x::Vector{Point{d}}, w::Float64) where d
- 
+function outer(λn_weight_vector::Vararg{Point{2}})
+    return [prod(e) for e in Iterators.product(λn_weight_vector...)]
+end
 
-    if d==1
-        N,n0 = size(A)
-        δ0 = 1.0./(n0-1.0)
-        for n in 1:N
-            x0 = x[n][1]
-            x0 = min.(max.(x0, 0.0),1.0)
-            q0 = div.(x0, δ0)
-            q0 = max.(0, q0)
-            q0 = min.(q0, n0-2)
-            λ0 = (x0./δ0-q0) # ∈[0,1[ by construction
-            q0_ = round.(Int,q0) + 1
-            A[n, q0_]   += (1-λ0)*w
-            A[n, q0_+1] += λ0*w
-        end
+function indexes_to_be_modified(qn_,n::Int64)
+    return tuple(n, UnitRange.(qn_,qn_.+1)...)
+end
 
-    elseif d==2
-        N,n0,n1 = size(A)
-        vector_n = SVector{2,Float64}(n0,n1)
-        δ = SVector{2,Float64}(1.0./(n0-1.0), 1.0./(n1-1.0))
-        for n in 1:N
+function fill_transition_matrix!(A, qn_, λn::Point{d}, w::Float64, n::Int64) where d
+    #A[indexes_to_be_modified(qn_,n)...] .+= w.*outer(make_λn_weight_vector(λn)...)
+    B = A[indexes_to_be_modified(qn_,n)...]
+end
 
-            xn = x[n]
-            xn = min.(max.(xn, 0.0),1.0)
-            qn = div.(xn, δ)
-            qn = max.(0, qn)
-            qn = min.(qn, vector_n.-2)
-            λn = (xn./δ.-qn) # ∈[0,1[ by construction
-            qn_ = round.(Int,qn) + 1
-
-            q0_, q1_ = qn_[1], qn_[2]
-            λ0, λ1  = λn[1], λn[2]
-            A[n, q0_ ,  q1_] += (1-λ0)*(1-λ1)*w
-            A[n, q0_+1, q1_] += λ0*(1-λ1)*w
-            A[n, q0_, q1_+1] += (1-λ0)*λ1*w
-            A[n, q0_+1, q1_+1] += λ0*λ1*w
-        end
+function my_trembling_hand!(A, x::Vector{Point{d}}, w::Float64) where d
     
-    else 
-        print("Error : grid's dimension limited to 2")
+    @assert ndims(A) == d+1
+    shape_A = size(A)
+    grid_dimension = d
+    δ =  SVector{d,Float64}(1.0./(shape_A[1+i]-1) for i in 1:d ) #@SVector[1.0./(shape_A[1+i]-1) for i in 1:(ndims(A)-1)]# SVector( 1.0./(shape_A[1+i]-1) for i in 1:d )
+    
+
+    for n in 1:shape_A[1]
+
+        xn = x[n]
+        xn = min.(max.(xn, 0.0),1.0)
+        qn = div.(xn, δ)
+        qn = max.(0, qn)
+        qn = min.(qn, shape_A[2:d+1].-2)
+        λn = (xn./δ.-qn) # ∈[0,1[ by construction
+        qn_ = round.(Int,qn) + 1
+        
+        
+        fill_transition_matrix!(A,qn_,λn,w,n)
+        
     end
 
 end
