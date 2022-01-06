d5c92eeb669df60deccb0eb89ac7bfeb6a521e9d
Gabrielle Queran <gabrielle.queran@gmail.com>
2022-01-06 15:53:16 +0100

    modifying the second test

--- test/outer.jl
+++ test/outer.jl
@@ -3,12 +3,18 @@ using Distributions
 using StaticArrays
 using Dolo
 
-@testset "testing the function outer of ergodic.jl" begin
+@testset "Tests on outer product functions" begin
 
-    λ1, λ2 = rand(Uniform(0,1),2)
+    @testset "testing the function outer of ergodic.jl" begin
 
-    @test Dolo.outer(SVector(1-λ1,λ1),SVector(1-λ2, λ2)) == @SMatrix[(1-λ1)*(1-λ2) (1-λ1)*λ2; λ1*(1-λ2) λ1*λ2]
-    @test Dolo.outer(tuple( (SVector(1-λ1,λ1) for i in 1:1)... )...) == @SVector(1-λ1,λ1)
+        λ1, λ2 = rand(Uniform(0, 1), 2)
 
-    
+        @test Dolo.outer(SVector(1 - λ1, λ1), SVector(1 - λ2, λ2)) == @SMatrix [(1-λ1)*(1-λ2) (1-λ1)*λ2; λ1*(1-λ2) λ1*λ2]
+        @test Dolo.outer(SVector(1 - λ1, λ1)) == @SVector(1 - λ1, λ1)
+
+    end
+
+    # @testset "testing the function outer2 of ergodic.jl" begin
+        
+    # end
 end
