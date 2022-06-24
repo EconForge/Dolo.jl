b290d05ea92a538f2b6fa6a0e822e8a73c5c11b3
Winant Pablo <pablo.winant@gmail.com>
2022-06-24 08:48:03 +0200

    EGM models (WIP)

--- src/model.jl
+++ src/model.jl
@@ -498,8 +498,8 @@ import Dolang: stringify
 function get_factories(model::Model)
 
     facts = Dict()
-    for eq_type in ["transition", "arbitrage"]
-        if eq_type == "transition"
+    for eq_type in keys(model.data["equations"])
+        if eq_type != "arbitrage"
             factories = (get_factory(model, eq_type), )
         else
             factories = get_factory(model, eq_type)
@@ -514,6 +514,7 @@ function get_factories(model::Model)
 end
 
 function get_factory(model::Model, eq_type::String)
+
     if eq_type == "arbitrage"
         defs_0 = get_definitions(model; stringify=true)
         defs_1 = get_definitions(model; tshift=1, stringify=true)
@@ -562,23 +563,46 @@ function get_factory(model::Model, eq_type::String)
 
         return (ff, ff_lb, ff_ub)
            
+    # elseif eq_type=="transition"
+    #     # defs_0  = get_definitions(model; stringify=true)
+    #     defs_m1 = get_definitions(model; tshift=-1, stringify=true)
+
+    #     definitions = OrderedDict{Symbol, SymExpr}([ (Dolang.stringify(k), v) for (k,v) in  defs_m1 ])
+
+    #     equations = get_assignment_block(model, eq_type)
+    #     symbols = get_symbols(model)
+    #     arguments = OrderedDict(
+    #         :m => [stringify(e,-1) for e in symbols[:exogenous]],
+    #         :s => [stringify(e,-1) for e in symbols[:states]],
+    #         :x => [stringify(e,-1) for e in symbols[:controls]],
+    #         :M => [stringify(e,0) for e in symbols[:exogenous]],
+    #         
+    #     )
+    #     ff = FunctionFactory(equations, arguments, definitions, Symbol(eq_type))
+    #     return ff
     else
-        # defs_0  = get_definitions(model; stringify=true)
-        defs_m1 = get_definitions(model; tshift=-1, stringify=true)
-
-        definitions = OrderedDict{Symbol, SymExpr}([ (Dolang.stringify(k), v) for (k,v) in  defs_m1 ])
-
+        specs = RECIPES[:dtcc][:specs]
+        if !(Symbol(eq_type) in keys(specs))
+            error("No spec for equation type '$eq_type'")
+        end
+        recipe = specs[Symbol(eq_type)]
+        if !(:target in keys(recipe))
+            error("Not implemented")
+        end
         equations = get_assignment_block(model, eq_type)
         symbols = get_symbols(model)
-        arguments = OrderedDict(
-            :m => [stringify(e,-1) for e in symbols[:exogenous]],
-            :s => [stringify(e,-1) for e in symbols[:states]],
-            :x => [stringify(e,-1) for e in symbols[:controls]],
-            :M => [stringify(e,0) for e in symbols[:exogenous]],
-            :p => [stringify(e) for e in symbols[:parameters]]
+        times = unique([e[2] for e in recipe[:eqs]])
+        defs = [get_definitions(model; tshift=t, stringify=true) for t in times]
+        definitions = OrderedDict{Symbol, SymExpr}(
+            [ (Dolang.stringify(k), v) for (k,v) in merge(defs...)]
         )
+        arguments = OrderedDict(
+            Symbol(l[3]) => [stringify(e,l[2]) for e in symbols[Symbol(l[1])]]
+            for l in recipe[:eqs] if !(l[1]=="parameters")
+        )
+        arguments[:p] = [stringify(e) for e in symbols[:parameters]]
+
         ff = FunctionFactory(equations, arguments, definitions, Symbol(eq_type))
-        return ff
     end
 
 end
