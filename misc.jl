8962b6e2befbd24223955dd6444ae2d8bd7c2bb7
Pablo Winant <pablo.winant@gmail.com>
2024-05-06 22:45:14 +0200

    WIP

--- misc.jl
+++ misc.jl
@@ -1,26 +1,47 @@
 using Dolo
 
 root_dir = pkgdir(Dolo)
+model = include("$(root_dir)/examples/ymodels/rbc_mc.jl")
+
+
+
+@time Dolo.time_iteration(model; verbose=true, engine=:cpu);
+@time Dolo.time_iteration(model; verbose=true, improve=true, improve_K=1000, improve_wait=0);
+@time Dolo.time_iteration(model; verbose=true, improve=true, improve_K=2, interpolation=:cubic);
+
+
+
+
+using Dolo
+
+root_dir = pkgdir(Dolo)
 model = include("$(root_dir)/examples/ymodels/rbc_iid.jl")
 
-isbits(model)
+
+@time Dolo.time_iteration(model; verbose=false);
+@time Dolo.time_iteration(model; verbose=false, engine=:cpu);
+# @time Dolo.time_iteration(model; verbose=false, engine=:gpu);
 
 
-Dolo.time_iteration(model; verbose=false);
-Dolo.time_iteration(model; verbose=false, improve=true, improve_K=1000);
-@time Dolo.time_iteration(model; verbose=false, improve=true, improve_K=1000, interpolation=:cubic);
+@time Dolo.time_iteration(model; verbose=true, improve=true, improve_K=1000, improve_wait=0);
+@time Dolo.time_iteration(model; verbose=true, improve=true, improve_K=2, interpolation=:cubic);
 
+####
+####
+####
+
+root_dir = pkgdir(Dolo)
+model = include("$(root_dir)/examples/ymodels/rbc_ar1.jl")
 
 
 dmodel = Dolo.discretize(model)
-
 wksp = Dolo.time_iteration_workspace(dmodel; interp_mode=:linear);
 res = Dolo.time_iteration(dmodel, wksp; verbose=false, improve=false, improve_K=1000, engine=:default);
-
+res
 
 wksp = Dolo.time_iteration_workspace(dmodel; interp_mode=:linear);
-res = Dolo.time_iteration(dmodel, wksp; verbose=false, improve=false, improve_K=1000, engine=:default, trace=true);
-
+res = Dolo.time_iteration(dmodel, wksp; verbose=true, improve=false, improve_K=1000, engine=:default, trace=true);
+res
 
 
 @time Dolo.time_iteration(dmodel, wksp; verbose=false, improve=false, improve_K=1000, engine=:default);
@@ -29,8 +50,6 @@ res = Dolo.time_iteration(dmodel, wksp; verbose=false, improve=false, improve_K=
 
 
 @time Dolo.time_iteration(dmodel; verbose=true, improve=true, improve_wait=0, improve_K=500);
-
-@time Dolo.time_iteration(dmodel; verbose=true, improve=true, improve_wait=0, improve_K=500, interpolation=:cubic);
 
 
 
